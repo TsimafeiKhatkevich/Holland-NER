8e587292502c7e8d6940ec0a48a6e9dd4c086e6e
yasek <serge.yaskevich@gmail.com>
2012-11-20 00:40:43 +0300

    next prev

--- get_info.py
+++ get_info.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from collections import defaultdict
+import cgi
 import sys
 
 LABELS = ("B-LOC", "B-MISC","B-ORG","B-PER", "I-LOC", "I-MISC","I-ORG","I-PER", "O")
@@ -25,21 +26,21 @@ def print_stats(data, output):
 def print_mistakes(data, output):
     output.write("<table border='1'>")
     output.write("<tr>")
-    for i in ("i", "word", "answer", "correct"):
+    for i in ("i", "prev", "word", "next", "answer", "correct"):
         output.write("<th>%s</th>" % str(i))
     output.write("</tr>")
     
     for row in data:
         output.write("<tr>")
         for cell in row:
-            output.write("<td>%s</td>" % str(cell))
+            output.write("<td>%s</td>" % cgi.escape(str(cell)))
         output.write("</tr>")
 
     output.write("</table>")
 
 
 def row_cmp(a, b):
-    return cmp(a[3],b[3]) or cmp(a[1], b[1]) 
+    return cmp(a[5],b[5]) or cmp(a[2], b[2]) 
 
 
 def main(result_name, answer_name, test):
@@ -62,7 +63,7 @@ def main(result_name, answer_name, test):
     for i, p in enumerate(comparison_chain):
         stat[p] += 1
         if p[0] != p[1]:
-            incorrect.append(tuple([i, words[i], p[0], p[1]]))
+            incorrect.append(tuple([i, words[i-1],words[i], words[i+1], p[0], p[1]]))
 
     incorrect.sort(row_cmp)
 
