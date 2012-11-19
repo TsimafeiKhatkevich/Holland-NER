2429de790f93c612e620882d4888a18b8fbd2a9b
yasek <serge.yaskevich@gmail.com>
2012-11-19 23:23:46 +0300

    added key -t for not running test part

--- easy_test.sh
+++ easy_test.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
-./run.sh -f data/dutch.train.clean.txt -c 5 -i 200 -g 3.5 -m dutch > data/dutch.train.evaluated.txt
+do_not_test=0
+for var in "$@"
+do
+    case "$var" in
+    -t*) do_not_test=1
+        ;;
+    esac
+done
+
+if (( $do_not_test == 0 ))
+then
+    ./run.sh -f data/dutch.train.clean.txt -c 5 -i 200 -g 3.5 -m dutch > data/dutch.train.evaluated.txt
+fi
 
 ./get_info.py "data/dutch.train.evaluated.txt" "data/dutch.train.results.txt" "data/dutch.train.clean.txt"
 
