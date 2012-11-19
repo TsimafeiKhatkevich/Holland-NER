c80c396bbdac01c95ffd1e05a4e3c9488639b62b
yasek <serge.yaskevich@gmail.com>
2012-11-20 01:01:15 +0300

    new options: -t do testing, -T do Training, -v make tables

--- easy_test.sh
+++ easy_test.sh
@@ -1,21 +1,34 @@
 #!/usr/bin/env bash
-
-do_not_test=0
+do_train=0
+do_test=0
+do_viz=0
 evaluated="data/dutch.train.evaluated.txt"
 results="data/dutch.train.results.txt" 
 clean="data/dutch.train.clean.txt" 
 for var in "$@"
 do
     case "$var" in
-    -t*) do_not_test=1
+    -t*) do_test=1
+        ;;
+    -T*) do_train=1
+        ;;
+    -v*) do_viz=1
         ;;
     esac
 done
 
-if (( $do_not_test == 0 ))
+if (( $do_train == 1 ))
+then
+    ./run.sh -f data/dutch.train.txt -c 5 -i 200 -g 3.5 -m dutch -T
+fi
+
+if (( $do_test == 1 ))
 then
     ./run.sh -f data/dutch.train.clean.txt -c 5 -i 200 -g 3.5 -m dutch > $evaluated
 fi
 
-./get_info.py $evaluated $results $clean 
+if (( $do_viz == 1 ))
+then
+    ./get_info.py $evaluated $results $clean 
+fi
 
